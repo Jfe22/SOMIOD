0bccebd90bae89abd8cf529016a1ead2cf2c34ed
Jfe22 <2181706@my.ipleiria.pt>
2023-12-27 17:47:46 +0000

    Lighting app minus messaging system

--- SOMIOD/Lighting/Form1.cs
+++ SOMIOD/Lighting/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using RestSharp;
+using SOMIOD.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -15,6 +16,9 @@ namespace Lighting
     public partial class Form1 : Form
     {
 
+
+        //can we use the models like this??? had to add a reference to SOMIOD and I think I shouldn't
+
         string baseURI = @"http://localhost:49760/api/somiod/";
         RestClient restClient = null;
 
@@ -26,17 +30,51 @@ namespace Lighting
 
         private void buttonConnectApp_Click(object sender, EventArgs e)
         {
+            RestRequest requestApp = new RestRequest("lighting", Method.Get);
+            RestResponse responseApp = restClient.Execute(requestApp);
 
-            //fazer pedido get a application lightbulb
-            
-            //se nao existir fazer post e criar
+            if (responseApp.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                //create app resource
+                SOMIOD.Models.Application application = new SOMIOD.Models.Application
+                {
+                    Name = "Lighting"
+                };
+                RestRequest requestCreateApp = new RestRequest("applications", Method.Post);
+                requestCreateApp.AddObject(application);
+                RestResponse responseCreateApp = restClient.Execute(requestCreateApp);
+                if (responseCreateApp.StatusCode != System.Net.HttpStatusCode.OK) return;
 
-            //no final fazer a ligacao como receiver nos web sockets?
+                //create container resource
+                SOMIOD.Models.Container container = new SOMIOD.Models.Container
+                {
+                    Name = "light_bulb"
+                };
+                RestRequest requestCreateCont = new RestRequest("lighting/containers", Method.Post);
+                requestCreateCont.AddObject(container);
+                RestResponse responseCreateCont = restClient.Execute(requestCreateCont);
+                if (responseCreateApp.StatusCode != System.Net.HttpStatusCode.OK) return;
 
-            //por motivos de teste, aproveitar ja este butao para apanhar o valor do light bulb e meter na
-            //box???
+                //create subscription resource
+                Subscription subscription = new Subscription
+                {
+                    Name = "sub1",
+                    Event = "pdahahaha",
+                    Endpoint = "poragorasla",
+                };
+                RestRequest requestCreateSub = new RestRequest("lighting/lighting_bulb/subscriptions", Method.Post);
+                requestCreateSub.AddObject(subscription);
+                RestResponse responseCreateSub = restClient.Execute(requestCreateCont);
+            }
 
+            RestRequest requestData = new RestRequest("lighting/light_bulb/data/1", Method.Get);
+            requestData.RequestFormat = DataFormat.Xml;
+            //var responseData = restClient.Execute<Data>(requestData).Data;
+            Data responseData = restClient.Execute<Data>(requestData).Data;
+            if (responseData == null) textBoxLightValue.Text = "No data yet";
+            else textBoxLightValue.Text = responseData.Content;
 
+            //turn the websocket on here as a listener???
 
         }
     }
